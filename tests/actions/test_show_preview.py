c19379de3b6b8d0628ff591a9f15f9340910fb25
edgar <edgar.dev22@gmail.com>
2024-05-03 09:06:08 -0300

    Req03

--- tests/actions/test_show_preview.py
+++ tests/actions/test_show_preview.py
@@ -1,1 +1,39 @@
-from pro_filer.actions.main_actions import show_preview  # NOQA
+from pro_filer.actions.main_actions import show_preview
+
+
+def test_show_preview_with_files_and_dirs(capsys):
+    context = {
+        "all_files": [
+            "src/__init__.py",
+            "src/app.py",
+            "src/utils/__init__.py",
+            "src/utils/helpers.py",
+            "src/utils/constants.py",
+            "src/utils/config.py"
+        ],
+        "all_dirs": [
+            "src",
+            "src/utils"
+        ]
+    }
+
+    show_preview(context)
+    captured = capsys.readouterr()
+    assert captured.out == (
+        "Found 6 files and 2 directories\n"
+        "First 5 files: ['src/__init__.py', 'src/app.py', "
+        "'src/utils/__init__.py', 'src/utils/helpers.py', "
+        "'src/utils/constants.py']\n"
+        "First 5 directories: ['src', 'src/utils']\n"
+    )
+
+
+def test_show_preview_with_empty_files_and_dirs(capsys):
+    context = {
+        "all_files": [],
+        "all_dirs": []
+    }
+
+    show_preview(context)
+    captured = capsys.readouterr()
+    assert captured.out == "Found 0 files and 0 directories\n"
