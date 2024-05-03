5fd82d27b1b9851de7d8647b9e7587ea04ad17dc
edgar <edgar.dev22@gmail.com>
2024-05-03 09:33:45 -0300

    Req04:feat

--- tests/actions/test_show_details.py
+++ tests/actions/test_show_details.py
@@ -1,61 +1,43 @@
-from datetime import date
-import pytest
 from pro_filer.actions.main_actions import show_details
+import os
+from datetime import date
 
 
-def test_show_details_existing_file(capfd):
-    context = {
-        "base_path": "/home/trybe/Downloads/Trybe_logo.png"
-    }
+def test_show_details(dir, capsys):
+    context = {"base_path": dir}
     show_details(context)
-    out, _ = capfd.readouterr()
-    assert "File name: Trybe_logo.png" in out
-    assert "File size in bytes" in out
-    assert "File type: file" in out
-    assert "File extension: .png" in out
-    assert "Last modified date:" in out
+    captured = capsys.readouterr()
+    expected_output = (
+        f"File name: file.txt\n"
+        f"File size in bytes: 23\n"
+        f"File type: file\n"
+        f"File extension: .txt\n"
+        f"Last modified date: {date.fromtimestamp(os.path.getmtime(dir))}\n"
+    )
+    assert captured.out.strip() == expected_output.strip()
 
 
-def test_show_details_nonexistent_file(capfd):
-    context = {
-        "base_path": "/home/trybe/????"
-    }
+def test_show_details_without_extension(tmp_path, capsys):
+    file = tmp_path / "without_extension"
+    file.write_text("GABIGOL SE LESIONOU")
+    context = {"base_path": str(file)}
     show_details(context)
-    out, _ = capfd.readouterr()
-    assert "File '????' does not exist" in out
+    captured = capsys.readouterr()
+    expected_output = (
+        f"File name: without_extension\n"
+        f"File size in bytes: 19\n"
+        f"File type: file\n"
+        f"File extension: [no extension]\n"
+        f"""Last modified date: {
+            date.fromtimestamp(os.path.getmtime(str(file)))
+            }\n"""
+    )
+    assert captured.out.strip() == expected_output.strip()
 
 
-def test_show_details_directory(capfd):
-    context = {
-        "base_path": "/home/trybe/Documents"
-    }
+def test_show_details_file_not_found(capsys):
+    context = {"base_path": "/path/to/none/file_not_found"}
     show_details(context)
-    out, _ = capfd.readouterr()
-    assert "File type: directory" in out
-
-
-def test_show_details_no_extension(capfd):
-    context = {
-        "base_path": "/home/trybe/Downloads/README"
-    }
-    show_details(context)
-    out, _ = capfd.readouterr()
-    assert "File extension: [no extension]" in out
-
-
-def test_show_details_date_format(capfd):
-    context = {
-        "base_path": "/home/trybe/Downloads/Trybe_logo.png"
-    }
-    show_details(context)
-    out, _ = capfd.readouterr()
-    date_str = out.split("Last modified date: ")[1].strip()
-    try:
-        date.fromisoformat(date_str)
-        assert True
-    except ValueError:
-        assert False, "Date format is incorrect"
-
-
-if __name__ == "__main__":
-    pytest.main()
+    captured = capsys.readouterr()
+    expected_output = "File 'file_not_found' does not exist\n"
+    assert captured.out.strip() == expected_output.strip()
